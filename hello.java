107d9fc68ccce1c0d94d24cf0355fe7abb52dae6
DLB <dlb@MacBook-Pro-DLB.Dlink>
2017-09-21 22:37:01 +0300

    Add jhdf

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) { //entry point
-        System.out.println("Hello, World!");
+        System.out.println("Hello, nekit");
     }
 }
