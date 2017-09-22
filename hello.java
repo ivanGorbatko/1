a2086513d08f86d75f4f20de84e3ea8b42527aea
ivanGorbatko <32130861+ivanGorbatko@users.noreply.github.com>
2017-09-22 23:25:56 +0300

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) { //entry point
-        System.out.println("Hello, серега пидр nekit");
+        System.out.println("Hello,  cthtuf gbljhсерега пидр nekit");
     }
 }
